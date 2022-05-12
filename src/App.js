ac4c56809ab28eaabef655a2d9dbdbe440858d04
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-12 11:11:30 +0200

    props for sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import {useState} from 'react';
 import './App.css';
 import Main from './Main.js'
 import Sidebar from './Sidebar.js'
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+    const newNote = {
+      id:
+      title:
+      body:
+      lastModified:
+    }
+  };
+
   return (
   <div className="App">
-    <Sidebar />
+    <Sidebar notes={notes} onAddNote={onAddNote} />
     <Main />
   </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,12 @@
-function Sidebar() {
+function Sidebar({notes, onAddNote}) {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>Notes</h1>
-        <button>Add</button>
+        <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
+      {notes.map((note) => (
         <div className="app-sidebar-note">
           <div className="app-sidebar-note-title">
             <strong>Title</strong>
@@ -14,6 +15,7 @@ function Sidebar() {
           <p>Preview</p>
           <small className="note-meta">Last modified [date]</small>
         </div>
+        ))}
       </div>
     </div>
     );
